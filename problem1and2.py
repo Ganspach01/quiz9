89a6d647d1e4147c92b6974da4106245a0f43d14
Grace Anspach <123026604+Ganspach01@users.noreply.github.com>
2023-04-28 11:44:23 -0400

    Add files via upload

---
+++ problem1and2.py
@@ -0,0 +1,12 @@
+#Grace Anspach
+#Problem 1
+fout=open("aproblem.txt","w")
+line1= "This is line 1"
+line2= "This is line 2"
+line3= "This is line 3"
+line4= "This is line 4"
+line5= "This is line 5"
+
+#Problem 2
+in line in files:
+    print(lines)
---
+++ problem3and4.py
@@ -0,0 +1,8 @@
+#Grace Anspach
+import dbm
+Houses=dbm.open("somehouses","r")
+Houses=("bed.png":"a 2 bed")
+Houses=("bath.png":"3 bath Victorian house in South Bend")
+Houses=("rooms.png":"the house has 5 rooms")
+Houses=("basement.png":"there is a basement")
+Houses=("attic.png":"there is not an attic")
